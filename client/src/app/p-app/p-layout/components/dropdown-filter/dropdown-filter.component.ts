8301b74ce5c52fd3da8b83b77dd7f6909d595cc2
MinhHai <minhhaitrandang@gmail.com>
2023-06-21 23:01:36 +0700

    filter-list-product-add: thêm component  dropdownfilter bản demo  chưa xử lý xong logic filter khi checked

--- client/src/app/p-app/p-layout/components/dropdown-filter/dropdown-filter.component.ts
+++ client/src/app/p-app/p-layout/components/dropdown-filter/dropdown-filter.component.ts
@@ -1,47 +1,77 @@
-import { Component, ElementRef, Input, Renderer2, ViewChild } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
-  selector :"dropdown-filter",
-  templateUrl : "./dropdown-filter.component.html",
-  styleUrls :['./dropdown-filter.component.scss']
+  selector: 'dropdown-filter',
+  templateUrl: './dropdown-filter.component.html',
+  styleUrls: ['./dropdown-filter.component.scss'],
 })
-export class dropdownFilterComponent{
-  constructor(private renderer:Renderer2){}
-  @ViewChild('myDropdownList') myDropdownList :ElementRef
-  @Input() filterbyPrice : boolean = false
-  @Input() activeSearch : boolean= false
-  @Input() titleFilter :string = "default"
-  @Input() placeholderSearch : string = `Tìm ${this.titleFilter}`
-  @Input() itemsDropList :any[] = [
+export class dropdownFilterComponent {
+  constructor(private renderer: Renderer2) {}
+  @ViewChild('myDropdownList') myDropdownList: ElementRef;
+  @Input() filterbyPrice: boolean = false;
+  @Input() activeSearch: boolean = false;
+  @Input() titleFilter: string = 'default';
+  @Input() placeholderSearch: string = `Tìm ${this.titleFilter}`;
+  @Input() itemsDropList: any[] = [
     {
-      type : "checkbox",
-      inputValue : "",
-      name : "cc"
+      type: 'checkbox',
+      inputValue: 'field value',
+      field: 'query field',
+      lableName: 'lable',
     },
     {
-      type : "checkbox",
-      inputValue : "",
-      name : "cc"
+      type: 'checkbox',
+      inputValue: 'field value',
+      field: 'query field',
+      lableName: 'lable',
     },
     {
-      type : "checkbox",
-      inputValue : "",
-      name : "cc"
-    }
-  ]
-  active : boolean = false
+      type: 'checkbox',
+      inputValue: 'field value',
+      field: 'query field',
+      lableName: 'lable',
+    },
+  ];
+
+  @Output() filterURL = new EventEmitter<string>();
+  @Output() queryFilter = new EventEmitter<any[]>();
+  active: boolean = false;
+  query: string = '';
+  lableFilter: any[] = [];
   ngOnInit(): void {
     if (this.filterbyPrice) {
-      this.titleFilter ="Mức giá"
+      this.titleFilter = 'Mức giá';
     }
-
   }
-  clickBoxFilter(){
+  clickBoxFilter() {
     const element = this.myDropdownList.nativeElement;
-    const hasClass = element.classList.contains('active-dropdown-list')
-    hasClass? this.active = false : this.active = true
+    const hasClass = element.classList.contains('active-dropdown-list');
+    hasClass ? (this.active = false) : (this.active = true);
     // console.log("hasClass",hasClass);
     // console.log("active",this.active);
   }
+  handleCheck(event: any) {
+    console.log(event.target.checked);
+    event.target.checked
+      ? (this.query += `&${event.target.name}=${event.target.value}`)
+      : (this.query = this.query.replace(
+          `&${event.target.name}=${event.target.value}`,
+          ''
+        ));
+    this.filterURL.emit(this.query);
+    this.lableFilter.push(
+      this.itemsDropList[parseInt(event.target.id)].lableName
+    );
+    console.log(this.lableFilter);
 
+    this.queryFilter.emit(this.lableFilter);
+  }
 }
---
+++ client/src/app/p-app/p-layout/shared/dto/DTOProduct.ts
@@ -0,0 +1,14 @@
+export class DTOProduct {
+  productID: number;
+  stock: number;
+  name: string;
+  favorite: number;
+  categoryID: number;
+  price: number;
+  brandID: number;
+  image: string;
+  sale: number;
+  description: string;
+  createdAt: string;
+  constructor() {}
+}
