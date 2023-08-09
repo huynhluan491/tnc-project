3cc2938f44dc8a804b9c9afb2f49ea0f36c3d4d7
huynhluan491 <huynhluan491@gmail.com>
2023-08-09 22:38:38 +0700

    no-msg

--- client/src/app/p-app/cart-checkout2/cart-checkout2.component.ts
+++ client/src/app/p-app/cart-checkout2/cart-checkout2.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, SkipSelf } from '@angular/core';
+import { Component, OnDestroy, OnInit, SkipSelf } from '@angular/core';
 import { StorageService } from '../p-layout/shared/services/storage.service';
 import { Subject, takeUntil } from 'rxjs';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DTOUser } from '../_models/DTOUser';
 
 @Component({
   selector: 'app-cart-checkout2',
   templateUrl: './cart-checkout2.component.html',
   styleUrls: ['./cart-checkout2.component.scss'],
 })
-export class CartCheckout2Component implements OnInit {
+export class CartCheckout2Component implements OnInit, OnDestroy {
   constructor(
     @SkipSelf() private storageService: StorageService,
     private formBuilder: FormBuilder
@@ -16,12 +17,40 @@ export class CartCheckout2Component implements OnInit {
 
   deliverForm!: FormGroup;
   isRegister: boolean = false;
-
+  total: number = 0;
   productList: number = 1;
+  currentUser: DTOUser;
+  isBindUserInfo: FormControl = new FormControl(false);
+  ngUnsubscribe$ = new Subject<void>();
 
   onSubmit() {}
 
   ngOnInit() {
+    this.loadDeliverForm();
+    this.currentUser = this.storageService.getUser();
+
+    const orders = this.storageService.getOrders().orders;
+    orders.forEach((product) => {
+      this.total += product.Price * product.Amount;
+    })
+
+    this.isBindUserInfo.valueChanges.pipe(takeUntil(this.ngUnsubscribe$)).subscribe(
+      res => {
+        if (this.isBindUserInfo.value && this.currentUser) {
+          this.deliverForm.patchValue({
+            name: this.currentUser.FullName,
+            email: this.currentUser.Email,
+            number: this.currentUser.Phone,
+            address: this.currentUser.Address,
+          });
+          console.log(this.deliverForm.value);
+      } else{
+        this.deliverForm.reset();
+      }
+    });
+  }
+
+  loadDeliverForm() {
     this.deliverForm = this.formBuilder.group({
       name: ['', [Validators.required]],
       email: ['', [Validators.required]],
@@ -29,13 +58,10 @@ export class CartCheckout2Component implements OnInit {
       address: ['', [Validators.required]],
       payment_method: ['', [Validators.required]],
     });
+  }
 
-    const userData = this.storageService.getUser();
-    this.deliverForm.patchValue({
-      name: userData.Name,
-      email: userData.Email,
-      number: userData.Phone,
-      address: userData.Address,
-    });
+  ngOnDestroy(): void {
+    this.ngUnsubscribe$.next();
+    this.ngUnsubscribe$.complete();
   }
 }
--- client/src/app/p-app/p-layout/components/slider-container/slider-container.component.ts
+++ client/src/app/p-app/p-layout/components/slider-container/slider-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 @Component({
   selector: 'app-slider-container',
   templateUrl: './slider-container.component.html',
@@ -16,6 +16,9 @@ export class SliderContainerComponent {
   @Input() type: string;
 
   slideConfig = {};
+
+  ngOnChanges(changes: SimpleChanges): void {
+  }
 
   ngOnInit(): void {
     this.slideConfig = {
